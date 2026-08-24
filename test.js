d3891ab862221d26e13bc14da827e73c5f77e57f
NgocHoa08 <ngoc@hoa.local>
2026-08-24 09:45:39 +0700

    Update tool du toan, bang tuyen bo dap ung va bien ban ban giao

---
+++ test.js
@@ -0,0 +1,5 @@
+
+    if (typeof XLSX === 'undefined' || !XLSX.version) {
+      document.write('<script src="https://cdn.jsdelivr.net/npm/xlsx-js-style@1.2.0/dist/xlsx.bundle.js"><\/script>');
+    }
+  
